3fa1c676fdc7dcd631e3eca39f46644e4efb0a72
momenmohsen967-alt <momenmohsen967@gmail.com>
2025-12-05 19:56:12 +0000

    all

---
+++ jobs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+JOB_TYPE = (
+    ('part_time','part_time'),
+    ('full_time','full_time'),
+)
+
+
+class Job(models.Model):
+    user=models.ForeignKey(User, related_name="created_by", null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=30, null=False, blank=False)
+    job_type = models.CharField(max_length=10, null=False, blank=False, choices=JOB_TYPE, default=JOB_TYPE[0][0])
+    location = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    responsibility = models.TextField(max_length=200, null=True, blank=True, default="none")
+    qualifications = models.TextField(max_length=200, null=True, blank=True, default="none")
+    vacancy = models.IntegerField()
+    salary = models.CharField(max_length=10, null=False, blank=False)
+    benefits = models.TextField(max_length=200, null=True, blank=True, default="none")
+    image = models.ImageField(null=True, blank=True, upload_to='jobs/')
+    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"created_by : {self.user.id} {self.title} {self.description[:15]}...."
