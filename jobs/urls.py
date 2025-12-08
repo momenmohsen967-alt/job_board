ae7fcc0beb694325da92b64db4c64d7fe0f41f23
momenmohsen967-alt <momenmohsen967@gmail.com>
2025-12-08 18:51:04 +0000

    update jobs

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views  import job_list
+from .views import job_list, job_details
+
+app_name = 'jobs'
 
 urlpatterns = [
     path('', job_list, name="job_list"),
+    path('<int:id>', job_details, name="job_details"),
 ]
-
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Job
 
 def job_list (request):
-    jobs = Job.objects.all()
+    jobs = Job.objects.all().only('id',  'image', 'title', 'location', 'job_type', 'created_at')
     return render(request, 'jobs.html', {"jobs":jobs})
+
+
+def job_details(request, **kwargs):
+    job = Job.objects.get(id=kwargs['id'])
+    return render(request, 'job_details.html', {"job":job})
